e61171a80f41b2b52f5fc7916b05d24814e63007
Quinton Harris <quinton.harris@gmail.com>
2025-09-28 23:50:42 -0700

    adde logo

---
+++ assets/javascript/weather.js
@@ -0,0 +1,9 @@
+// Yesler (47.601, -122.298):
+// https://api.weather.gov/gridpoints/SEW/126,68/forecast/hourly
+
+// Bremerton House (47.609, -122.709):
+// https://api.weather.gov/gridpoints/SEW/113,71/forecast/hourly
+
+// Ballard House (47.669, -122.363):
+// https://api.weather.gov/gridpoints/SEW/124,71/forecast/hourly
+
